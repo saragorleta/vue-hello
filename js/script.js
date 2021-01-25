6eb4384617abbadd7569dbc71ad278df81c643a9
Sara <saragorleta@yahoo.com>
2021-01-25 17:44:12 +0100

    aggiunto immagine

--- js/script.js
+++ js/script.js
@@ -12,3 +12,11 @@ var salutoColorato=new Vue ({
     color:'red'
   }
 });
+
+var salutoImmagine=new Vue ({
+  el: "#root3",
+  data: {
+    colore:'green',
+    immagine:'https://www.immaginigratis.biz/wp-content/uploads/2018/11/Buongiorno-201-800x670.jpg'
+  }
+});
